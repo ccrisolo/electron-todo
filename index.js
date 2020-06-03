bb327f1fa707d7643973e8d8f4f53c2bc84fc2ea
ccrisolo <ccrisolo3@gmail.com>
2020-06-03 16:37:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const electron = require("electron");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    // addWindow.loadURL(`file://${__dirname}/add.html`);
+    // addWindow.on('closed', () => addWindow = null);
+}
+
+//create a menu template
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New Todo",
+        click() { createAddWindow(); }
+      },
+      {
+        label: "Quit",
+        accelerator: 
+        process.platform === "darwin" 
+        ? "Command+Q" 
+        : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+if (process.platform === "darwin") {
+  menuTemplate.unshift({ label: "" });
+}
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      {
+        label: "Toggle Developer Tools",
+        accelerator:
+          process.platform === "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  });
+}
